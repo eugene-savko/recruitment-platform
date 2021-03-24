188ce31625ffc976ecbbdec4e0dae737e7a82ba3
Zheltok Mariya <masha.zheltok@gmail.com>
2021-03-24 21:29:50 +0300

    add fixing bugs

--- src/main/java/com/exadel/recruitmentPlatform/main/recruitmentplatform/RecruitmentPlatformApplication.java
+++ src/main/java/com/exadel/recruitmentPlatform/main/recruitmentplatform/RecruitmentPlatformApplication.java
@@ -18,19 +18,19 @@ public class RecruitmentPlatformApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RecruitmentPlatformApplication.class, args);
 	}
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-
-		};
-	}
+//	@Bean
+//	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+//		return args -> {
+//
+//			System.out.println("Let's inspect the beans provided by Spring Boot:");
+//
+//			String[] beanNames = ctx.getBeanDefinitionNames();
+//			Arrays.sort(beanNames);
+//			for (String beanName : beanNames) {
+//				System.out.println(beanName);
+//			}
+//
+//		};
+//	}
 
 }
