ba2658f5aeb108e4baaae553b11ad6d623863243
Baranov Igor <igor.82bv@mail.ru>
2021-03-29 23:30:45 +0300

    iss9-feat: Create component InputPhone

--- src/app/components/Layout/FormCV/WrapperForInputName/InputText.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { InputText } from './component/InputStyle';
-
-export const Input: React.FunctionComponent = () => (
-	<>
-		<InputText placeholder="Name" />
-	</>
-);
