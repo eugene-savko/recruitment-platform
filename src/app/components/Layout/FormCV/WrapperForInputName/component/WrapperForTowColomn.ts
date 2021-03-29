d9a559b4ac068a65c34926732f8cb2466a478454
Baranov Igor <igor.82bv@mail.ru>
2021-03-29 23:20:08 +0300

    iss9-fix: Change style

--- src/app/components/Layout/FormCV/WrapperForInputName/component/WrapperForTowColomn.ts
+++ src/app/components/Layout/FormCV/WrapperForInputName/component/WrapperForTowColomn.ts
@@ -4,4 +4,5 @@ import { Container as MaterialContainer } from '@material-ui/core';
 export const WrapperForTowColomn = styled(MaterialContainer)`
 	display: flex;
 	flex-direction: row;
+	justify-content: space-between;
 `;
