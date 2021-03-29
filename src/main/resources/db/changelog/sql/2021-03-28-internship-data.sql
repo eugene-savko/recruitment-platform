69b4c287c289a2d063bdb51de688a64820cad4e0
Valery1508 <58391822+Valery1508@users.noreply.github.com>
2021-03-29 21:11:07 +0300

    Deleted file 2021-03-28-internship-data.sql

--- src/main/resources/db/changelog/sql/2021-03-28-internship-data.sql
+++
@@ -1,2 +0,0 @@
---liquibase formatted sql
-INSERT INTO "internship" (name, description, deadline, start_date, end_date, status, english_level, primary_skill) VALUES ('Java', 'Java Internship description.....', '2021-03-20 12:00:00', '2021-05-01 12:00:00', '2021-07-01 12:00:00', 'is going', 'B2', 'Java');
