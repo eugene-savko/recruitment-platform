071e4b4063be23ec4d878fadd115f3d57892b12f
Yury Dunets <jasper.hale.dev@gmail.com>
2021-03-31 02:47:09 +0300

    test: New infinite scroll npm module

--- src/app/components/Filter/InternshipList/index.tsx
+++ src/app/components/Filter/InternshipList/index.tsx
@@ -1,18 +1,33 @@
 import { FilterContext } from 'app/context/FilterContext';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { uid } from 'react-uid';
 import InfiniteScroll from 'react-infinite-scroller';
 import { OtherCourses } from './components/OtherCourses';
 import { InternshipListWrappper } from './components/InternshipListWrapper';
 import { ListItem } from './ListItem';
+import { dataInternshipList } from '../dataInternshipList';
 
 export const InternshipList: React.FunctionComponent = () => {
 	const filterContext = useContext(FilterContext);
 	const dataList = [...filterContext.internshipList];
+
+	function handleData(quantity: number) {
+		const scrollData = dataInternshipList.slice(0, quantity);
+		return scrollData;
+	}
 	return (
 		<>
 			<InternshipListWrappper>
-				<InfiniteScroll>
+				<InfiniteScroll
+					loadMore={() => 10}
+					pageStart={0}
+					hasMore={true || false}
+					loader={
+						<div className="loader" key={0}>
+							Loading ...
+						</div>
+					}
+				>
 					{dataList.map((internship) => (
 						<ListItem
 							key={uid(internship.id)}
@@ -22,8 +37,8 @@ export const InternshipList: React.FunctionComponent = () => {
 							status={internship.status}
 						/>
 					))}
-					<OtherCourses>Загрузить больше предложений</OtherCourses>
 				</InfiniteScroll>
+				<OtherCourses>Загрузить больше предложений</OtherCourses>
 			</InternshipListWrappper>
 		</>
 	);
