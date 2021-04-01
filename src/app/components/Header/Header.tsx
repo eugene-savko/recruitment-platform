d9b66b0562713272a7ddfe7d53c9ea26090242d2
SiarhKul <SiarhKul@gmail.com>
2021-04-01 20:58:45 +0300

    init:Initialization component Header

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const Header = () => {
+	console.log('Header');
+	return <div>Header</div>;
+};
--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
 import { Auth } from 'app/components/Auth';
+import { Header } from 'app/components/Header/Header';
 
-export const HomePage: React.FunctionComponent = () => <Auth />;
+export const HomePage: React.FunctionComponent = () => (
+	<>
+		<Header />
+		<Auth />;
+	</>
+);
