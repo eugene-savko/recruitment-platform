aa4f9fa18d854839c8d41aedf6e40e824dbe01bc
Baranov Igor <igor.82bv@mail.ru>
2021-04-02 19:50:35 +0300

    fix: Update Legend component

--- src/app/components/Layout/FormCV/Legend/index.tsx
+++ src/app/components/Layout/FormCV/Legend/index.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 import { LegendStyled } from '../component/Legend';
 
 export const Legend: React.FunctionComponent = () => (
-	<LegendStyled>Отправьте свою заявку</LegendStyled>
+	<LegendStyled>Send your CV</LegendStyled>
 );
