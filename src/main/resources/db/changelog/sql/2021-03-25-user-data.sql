7f6cf1590fad4b4241fde3aa0101bf490dbc8af2
alexeyoverchenko <alexeyoverchenko@gmail.com>
2021-04-03 13:53:41 +0300

    edited user-table

--- src/main/resources/db/changelog/sql/2021-03-25-user-data.sql
+++ src/main/resources/db/changelog/sql/2021-03-25-user-data.sql
@@ -1,2 +1,2 @@
 --liquibase formatted sql
-INSERT INTO "user" (first_name, last_name, email, role, skype, country, phone, status) VALUES ('Joe', 'Black', 'joeblack@gmail.com', 'candidate', 'live:1245345', 'Minsk', '+375299465836', 'under consideration');
+INSERT INTO "user" (first_name, last_name, email, role, skype, country, phone) VALUES ('Joe', 'Black', 'joeblack@gmail.com', 'candidate', 'live:1245345', 'Minsk', '+375299465836');
