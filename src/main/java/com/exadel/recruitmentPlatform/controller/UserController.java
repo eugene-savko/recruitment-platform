eb951921ee23741597d025071bfa3e745dd65066
alexeyoverchenko <alexeyoverchenko@gmail.com>
2021-04-03 13:45:29 +0300

    added UserController

---
+++ src/main/java/com/exadel/recruitmentPlatform/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.exadel.recruitmentPlatform.controller;
+
+import com.exadel.recruitmentPlatform.dto.UserDto;
+import com.exadel.recruitmentPlatform.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping ("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/{id}")
+    public UserDto getUser(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+}
