10a53497a993930e190282e2cb27b13d40b93a96
Valery1508 <lera.zubritskaya00@gmail.com>
2021-04-06 12:37:46 +0300

    Merge branch 'main' of D:\!Exadel\recruitment-platform\recruitment-platform with conflicts.

---
+++ src/main/java/com/exadel/recruitmentPlatform/controller/InternshipController.java
@@ -0,0 +1,11 @@
+package com.exadel.recruitmentPlatform.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/internships")
+public class InternshipController {
+}
