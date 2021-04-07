3d4e9674f9ef32adaf2c7bc8562521e3020e376c
Anatoliy Litvinov <program.virus.login@gmail.com>
2021-04-08 02:49:37 +0300

    feat: Implement SendForm approximate architecture

---
+++ src/app/pages/HomePage/SendForm/Body/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Body: React.FunctionComponent = () => {
+	return <div>Body</div>;
+};
---
+++ src/app/pages/HomePage/SendForm/components/index.ts
@@ -0,0 +1,6 @@
+import Wrapper from './Wrapper';
+import Title from './Title';
+import Form from './Form';
+import Note from './Note';
+
+export { Wrapper, Title, Form, Note };
