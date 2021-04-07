f520043a08328b6eca452c97d31bad5a50fb3a0c
Anatoliy Litvinov <program.virus.login@gmail.com>
2021-04-08 02:59:21 +0300

    feat: Include Poppins font, weight - bold. Style Title component

--- src/app/pages/HomePage/SendForm/components/Title.ts
+++ src/app/pages/HomePage/SendForm/components/Title.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
-const Title = styled.h2``;
+const Title = styled.h2`
+	font-family: Poppins, sans-serif;
+	font-weight: bold;
+	font-size: 30px;
+	line-height: 30px;
+	color: #40bfef;
+`;
 
 export default Title;
