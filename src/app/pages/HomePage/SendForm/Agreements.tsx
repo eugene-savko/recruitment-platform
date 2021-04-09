fe807d496bc9b4fe07b525a2d4e7f692c61bbc4a
Baranov Igor <igor.82bv@mail.ru>
2021-04-09 06:48:44 +0300

    feat:Implement Agreements component style

--- src/app/pages/HomePage/SendForm/Agreements.tsx
+++ src/app/pages/HomePage/SendForm/Agreements.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
+import { Label, Input } from './components/index';
+
 export const Agreements: React.FunctionComponent = () => {
-	return <div>Agreements</div>;
+	return (
+		<React.Fragment>
+			<p>
+				<Label>
+					<Input type="checkbox" />
+					<span>
+						Подавая заявку на эту должность, я отправляю свои личные данные в
+						Exadel и даю согласие на обработку личных данных с целью приема на
+						работу.
+					</span>
+				</Label>
+			</p>
+		</React.Fragment>
+	);
 };
