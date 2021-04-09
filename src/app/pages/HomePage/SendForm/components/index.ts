83e59792c122301ef5ca15ad402a3d02098e22c8
Baranov Igor <igor.82bv@mail.ru>
2021-04-09 06:48:44 +0300

    feat:Implement Agreements component style

--- src/app/pages/HomePage/SendForm/components/index.ts
+++ src/app/pages/HomePage/SendForm/components/index.ts
@@ -3,5 +3,6 @@ import Title from './Title';
 import Form from './Form';
 import Note from './Note';
 import Input from './Input';
+import Label from './Label';
 
-export { Wrapper, Title, Form, Note, Input };
+export { Wrapper, Title, Form, Note, Input, Label };
