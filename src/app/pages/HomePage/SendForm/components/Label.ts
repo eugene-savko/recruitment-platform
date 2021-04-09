ff968970116014637707bad6686ed72827d25828
Baranov Igor <igor.82bv@mail.ru>
2021-04-09 06:48:44 +0300

    feat:Implement Agreements component style

---
+++ src/app/pages/HomePage/SendForm/components/Label.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Label = styled.label`
+	font-family: Roboto;
+	font-style: normal;
+	font-weight: normal;
+	font-size: 12px;
+	line-height: 14px;
+
+	color: #4e4e4e;
+
+	cursor: pointer;
+`;
+
+export default Label;
