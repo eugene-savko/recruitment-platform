827c74ac64a6848ac2bda5aeeb6d4dc2e2b45588
Katr1nD <kathrindrako@gmail.com>
2021-04-10 15:54:49 +0300

    create FAQs page

---
+++ src/app/pages/FAQsPage/Section/components/SectionWrapper.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const SectionWrapper = styled.section`
+	width: 100%;
+	max-width: 1000px;
+	margin: 0 auto;
+`;
---
+++ src/app/pages/FAQsPage/Section/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { SectionWrapper } from './components/SectionWrapper';
+
+type SectionProps = {
+	title: string,
+	text: string,
+	isOpened: boolean,
+	onClick: () => void,
+};
+
+export const Section: React.FunctionComponent<SectionProps> = ({
+	title,
+	text,
+	isOpened,
+	onClick,
+}) => (
+	<SectionWrapper onClick={onClick}>
+		<div className={`faq__question ${isOpened ? 'faq__question_opened' : ''}`}>
+			{title}
+		</div>
+		{isOpened && (
+			<div>
+				<p className="faq__answer">{text}</p>
+			</div>
+		)}
+	</SectionWrapper>
+);
