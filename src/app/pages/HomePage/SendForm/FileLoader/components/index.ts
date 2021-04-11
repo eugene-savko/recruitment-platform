827102d5710248cd52f68d53bf0b83e7c293af1f
Anatoliy Litvinov <program.virus.login@gmail.com>
2021-04-11 16:45:15 +0300

    feat: Implement AttachButton component style

---
+++ src/app/pages/HomePage/SendForm/FileLoader/components/index.ts
@@ -0,0 +1,6 @@
+import Wrapper from './Wrapper';
+import Button from './Button';
+import SelectedFile from './SelectedFile';
+import AttachButtonWrapper from './AttachButtonWrapper';
+
+export { Wrapper, Button, SelectedFile, AttachButtonWrapper };
