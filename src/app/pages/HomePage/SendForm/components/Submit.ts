37a3736498d06dd327961b8f24fd6d380b2ea8e6
Anatoliy Litvinov <program.virus.login@gmail.com>
2021-04-11 12:14:24 +0300

    fix: Fixed Button component style

--- src/app/pages/HomePage/SendForm/components/Submit.ts
+++ src/app/pages/HomePage/SendForm/components/Submit.ts
@@ -1,32 +1,29 @@
 import styled from 'styled-components';
 
 const Submit = styled.button`
-	width: 119px;
-	height: 36px;
-	padding: 10px 15px;
+	padding: 10px 20px;
 
-	background: #0082ca;
-	border-radius: 18px;
-	border: none;
-	color: #fff;
-
-	font-family: Open Sans, sans-serif;
-	font-size: 16px;
-	font-weight: 600;
-	font-style: normal;
-	line-height: 16px;
 	text-align: center;
 
+	background-color: #0082ca;
+	outline: none;
+
+	border-radius: 18px;
+	border: none;
+
+	font-family: 'Open Sans', sans-serif;
+	font-size: 16px;
+	font-weight: 600;
+	line-height: 16px;
+	color: #fff;
+
 	cursor: pointer;
-	outline-style: none;
+
+	transition: ease-in-out 0.2s;
 
 	&:hover {
 		background-color: #40bfef;
-		border: none;
-		box-shadow: 0 5px 10px rgb(37 37 37 / 10%);
-	}
-	&:focus {
-		border: 1px solid #fafafa;
+		transition: 0.2s ease-in-out;
 	}
 `;
 
