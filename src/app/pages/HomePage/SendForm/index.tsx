d24adcec4ad8676805096e4295cf947c6e19f678
Baranov Igor <igor.82bv@mail.ru>
2021-04-12 12:28:41 +0300

    feat: Implementation of data output from text fields

--- src/app/pages/HomePage/SendForm/index.tsx
+++ src/app/pages/HomePage/SendForm/index.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
+// style
 import { Form, Note, Title, Wrapper, Submit } from './components';
 
+// components
 import { Body } from './Body';
 import { FileLoader } from './FileLoader';
 import { Agreements } from './Agreements';
 
+// interface
+import IFormInputs from './types';
+
 export const SendForm: React.FunctionComponent = () => {
+	const { handleSubmit, register } = useForm<IFormInputs>();
+
+	// eslint-disable-next-line no-alert
+	const onSubmit = (data: IFormInputs) => alert(JSON.stringify(data));
+
 	return (
 		<Wrapper>
 			<Title>Submit your application</Title>
-			<Form>
-				<Body />
-
+			<Form onSubmit={handleSubmit(onSubmit)}>
+				<Body register={register} />
 				<Note>* Поля отмеченные * обязательны.</Note>
 				<FileLoader />
 				<Agreements />
-				<Submit>Submit</Submit>
+				<Submit type="submit">Submit</Submit>
 			</Form>
 		</Wrapper>
 	);
