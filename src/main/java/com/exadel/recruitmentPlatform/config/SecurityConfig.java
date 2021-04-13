97a76242a14fa390802956d143cd494daaefee52
Zheltok Mariya <masha.zheltok@gmail.com>
2021-04-14 00:29:38 +0300

    add security

---
+++ src/main/java/com/exadel/recruitmentPlatform/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.exadel.recruitmentPlatform.config;
+
+import com.exadel.recruitmentPlatform.service.Impl.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    public void setUserDetailsService(UserDetailsServiceImpl userDetailsService){
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        return authenticationProvider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .and().formLogin().permitAll()
+                .and().logout().permitAll();
+    }
+}
