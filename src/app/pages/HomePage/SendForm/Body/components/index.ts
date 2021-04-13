cb121b02cfbb46b758998f85808b7cc2106b8c62
Baranov Igor <igor.82bv@mail.ru>
2021-04-13 15:18:49 +0300

    feat: Create simple validate inputs

--- src/app/pages/HomePage/SendForm/Body/components/index.ts
+++ src/app/pages/HomePage/SendForm/Body/components/index.ts
@@ -3,5 +3,7 @@ import TextArea from './TextArea';
 import WrapperInputs from './WrapperInputs';
 import Input from './Input';
 import Select from './Select';
+import InputItem from './InputItem';
+import TextError from './TextError';
 
-export { Error, TextArea, WrapperInputs, Input, Select };
+export { Error, TextArea, WrapperInputs, Input, Select, InputItem, TextError };
