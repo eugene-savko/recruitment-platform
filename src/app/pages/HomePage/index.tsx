be619c9c15ba1b4ee4352625f874e60946722c3e
Baranov Igor <igor.82bv@mail.ru>
2021-04-16 16:27:06 +0300

    fix: Change path for Training component

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { SendForm } from './SendForm';
-
-// import { Training } from 'app/pages/HomePage/Trainings';
+import { Training } from './Trainings';
 
 export const HomePage: React.FunctionComponent = () => (
 	<React.Fragment>
-		{/* <Training /> */}
+		<Training />
 		<SendForm />
 	</React.Fragment>
 );
