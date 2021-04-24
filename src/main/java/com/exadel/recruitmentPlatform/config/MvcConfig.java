6edd4c5e14b68b385caec6fe2c0088e54f135ed7
Yauheni Sauko <ysauko@exadel.com>
2021-04-24 21:19:46 +0300

    Add allow headers for response

--- src/main/java/com/exadel/recruitmentPlatform/config/MvcConfig.java
+++ src/main/java/com/exadel/recruitmentPlatform/config/MvcConfig.java
@@ -11,7 +11,8 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("*");
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
 
