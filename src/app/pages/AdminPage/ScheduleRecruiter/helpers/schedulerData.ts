b2bebf287fbe7a071d42b1abbc1f81f1f4173879
SiarhKul <SiarhKul@gmail.com>
2021-04-25 13:56:27 +0300

    init: Creatde component scheduleRecruiter

---
+++ src/app/pages/AdminPage/ScheduleRecruiter/helpers/schedulerData.ts
@@ -0,0 +1,16 @@
+export const schedulerData = [
+	{
+		title: 'Website Re-Design Plan',
+		startDate: new Date(2021, 5, 25, 9, 35),
+		endDate: new Date(2021, 5, 25, 11, 30),
+		id: 0,
+		members: ['Ivan Graying', 'Ann Reading', 'Kate Blacking'],
+	},
+	{
+		title: 'Book Flights to San Fran for Sales Trip',
+		startDate: new Date(2021, 5, 25, 12, 11),
+		endDate: new Date(2021, 5, 25, 13, 0),
+		id: 1,
+		members: ['Ivan Graying', 'Ann Reading', 'Kate Blacking'],
+	},
+];
--- src/app/pages/AdminPage/ScheduleRecruiter/index.tsx
+++ src/app/pages/AdminPage/ScheduleRecruiter/index.tsx
@@ -1,5 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Paper from '@material-ui/core/Paper';
+import {
+	EditingState,
+	ViewState,
+	GroupingState,
+	IntegratedGrouping,
+	IntegratedEditing,
+} from '@devexpress/dx-react-scheduler';
+import {
+	Scheduler,
+	DayView,
+	WeekView,
+	Toolbar,
+	ViewSwitcher,
+	MonthView,
+	Appointments,
+	AppointmentTooltip,
+	AppointmentForm,
+	DateNavigator,
+	TodayButton,
+	Resources,
+	GroupingPanel,
+	DragDropProvider,
+} from '@devexpress/dx-react-scheduler-material-ui';
+
+import { schedulerData } from './helpers/schedulerData';
+import { owners } from './helpers/owners';
 
 export const ScheduleRecruiter: React.FunctionComponent = () => {
-	return <div>Page ScheduleRecruiter</div>;
+	// eslint-disable-next-line prefer-const
+	let [data, setData] = useState(schedulerData);
+
+	const currentDate = new Date(2021, 5, 25, 9, 35);
+
+	const commitChanges = ({ added, changed, deleted }: any) => {
+		if (added) {
+			const startingAddedId =
+				data.length > 0 ? data[data.length - 1].id + 1 : 0;
+			data = [...data, { id: startingAddedId, ...added }];
+			setData(data);
+		}
+		if (changed) {
+			data = data.map((appointment) =>
+				changed[appointment.id]
+					? { ...appointment, ...changed[appointment.id] }
+					: appointment
+			);
+			setData(data);
+		}
+		if (deleted !== undefined) {
+			data = data.filter((appointment) => appointment.id !== deleted);
+			setData(data);
+		}
+	};
+
+	const resources = [
+		{
+			fieldName: 'members',
+			title: 'Members',
+			instances: owners,
+			allowMultiple: true,
+		},
+	];
+	const grouping = [{ resourceName: 'members' }];
+
+	return (
+		<Paper>
+			<Scheduler data={data} height={850}>
+				<ViewState
+					defaultCurrentDate={currentDate}
+					defaultCurrentViewName="Week"
+				/>
+				<EditingState onCommitChanges={commitChanges} />
+				<GroupingState grouping={grouping} />
+
+				<DayView startDayHour={10} endDayHour={18} />
+				<WeekView startDayHour={10} endDayHour={18} />
+				<MonthView />
+
+				<Appointments />
+				<Resources data={resources} mainResourceName="members" />
+				<Toolbar />
+				<ViewSwitcher />
+
+				<IntegratedGrouping />
+				<IntegratedEditing />
+
+				<DateNavigator />
+				<TodayButton />
+				<AppointmentTooltip showCloseButton showOpenButton />
+				<AppointmentForm />
+
+				<GroupingPanel />
+				<DragDropProvider />
+			</Scheduler>
+		</Paper>
+	);
 };
