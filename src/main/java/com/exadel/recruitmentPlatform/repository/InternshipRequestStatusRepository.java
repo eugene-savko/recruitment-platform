0c599c69a3c62c71c0fedbad88a4b4af950b2cde
alexeyoverchenko <alexeyoverchenko@gmail.com>
2021-05-05 17:56:37 +0300

    - drop Status table - Modified status into the internshipRequest

--- src/main/java/com/exadel/recruitmentPlatform/repository/InternshipRequestStatusRepository.java
+++
@@ -1,7 +0,0 @@
-package com.exadel.recruitmentPlatform.repository;
-
-import com.exadel.recruitmentPlatform.entity.InternshipRequestStatusEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InternshipRequestStatusRepository extends JpaRepository<InternshipRequestStatusEntity, Long> {
-}
