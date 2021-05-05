f619724f1cf46be85fec09171406356f9a576124
alexeyoverchenko <alexeyoverchenko@gmail.com>
2021-05-05 12:22:05 +0300

    - deleted data from "status" table

--- src/main/resources/db/changelog/2021-05-02-fill-data-status.sql
+++ src/main/resources/db/changelog/2021-05-02-fill-data-status.sql
@@ -1,1 +1,5 @@
 INSERT INTO status (name) VALUES ('Pending'), ('Recruiter Feedback'), ('Technical Interview'), ('Technical Feedback'), ('Accepted'), ('Denied');
+
+DELETE from status;
+
+INSERT INTO status (name) VALUES ('Pending'), ('Recruiter Feedback'), ('Technical Interview'), ('Technical Feedback'), ('Accepted'), ('Denied');
