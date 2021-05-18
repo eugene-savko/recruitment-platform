f56389a8e7c31d052ceabc4aeb9d97f87ad7c7dc
Vados <Vados.Stradze@gmail.com>
2021-05-18 22:30:51 +0300

    remove @NotBlank

--- src/main/java/com/exadel/recruitmentPlatform/dto/EnglishLevelDto.java
+++ src/main/java/com/exadel/recruitmentPlatform/dto/EnglishLevelDto.java
@@ -5,15 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
-
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class EnglishLevelDto extends BaseDto {
-
-    @NotBlank
     private String englishLevel;
 
 }
