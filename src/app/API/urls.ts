bdfd011cac4d2a64f52584d810264d586270505d
Yury Dunets <jasper.hale.dev@gmail.com>
2021-05-19 01:17:21 +0300

    feat: add general context for Admin Page

--- src/app/API/urls.ts
+++ src/app/API/urls.ts
@@ -1,9 +1,6 @@
 const URL_INTERNSHIPS = 'api/internships/ids-names/';
 const URL_INFORMATION = 'api/information/';
+const URL_LOGIN = 'login';
+const URL_CURRENT_CANDIDATE = 'users/current';
 
-export { URL_INTERNSHIPS, URL_INFORMATION };
-const URL_LOGIN = 'https://recruitment-platform.herokuapp.com/login';
-const URL_CURRENT_CANDIDATE =
-	'https://recruitment-platform.herokuapp.com/users/current';
-
-export { URL_LOGIN, URL_CURRENT_CANDIDATE };
+export { URL_LOGIN, URL_CURRENT_CANDIDATE, URL_INTERNSHIPS, URL_INFORMATION };
