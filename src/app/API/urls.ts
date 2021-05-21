539521c3b50bb09a1ff7fb76f12622b0aa84414d
IgorBaranovDev <baranovigorjsdev@gmail.com>
2021-05-21 21:57:39 +0300

    update: Fixed urls

--- src/app/API/urls.ts
+++ src/app/API/urls.ts
@@ -1,6 +1,6 @@
 const URL_INTERNSHIPS = 'api/internships/ids-names/';
 const URL_INFORMATION = 'api/information/';
 const URL_LOGIN = 'login';
-const URL_CURRENT_CANDIDATE = 'users/current';
+const URL_CURRENT_CANDIDATE = 'api/users/current';
 
 export { URL_LOGIN, URL_CURRENT_CANDIDATE, URL_INTERNSHIPS, URL_INFORMATION };
